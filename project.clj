963e0df78877efaae4954d3ecbdf2d01597b9de0
Jani Rahkola <jani.rahkola@iki.fi>
2013-08-12 15:21:44 +0300

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject ad "0.1.0-SNAPSHOT"
+  :description "automatic differentiation"
+  :url "http://github.com/rapala/ad"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
